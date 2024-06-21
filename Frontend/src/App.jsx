a679de4e2f795bab72e65fb9575a2ba23efa8b51
agaberr <ahmedgaber312001@gmail.com>
2024-06-21 04:18:40 +0300

    Add icon - frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import LandingPage from './LandingPage/LandingPage'
+import SignUp from './SignUp/SignUp'
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <LandingPage />
+      <SignUp />
     </div>
   )
 }

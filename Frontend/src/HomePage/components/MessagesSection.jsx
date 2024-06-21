312f1de5dc6de168dbaed6c1d881f9630f80a794
agaberr <ahmedgaber312001@gmail.com>
2024-06-21 17:04:11 +0300

    Add chatNotSelected feature - frontend

--- Frontend/src/HomePage/components/MessagesSection.jsx
+++ Frontend/src/HomePage/components/MessagesSection.jsx
@@ -4,13 +4,30 @@ import Messages from './MessageSection/Messages'
 import SendMessage from './MessageSection/SendMessage'
 
 const MessagesSection = () => {
-  return (
-    <div>
-      <InfoBar />
-      <Messages />
-      <SendMessage />
-    </div>
-  )
+
+  const chatSelected = false;
+
+  if (chatSelected)  
+      return (
+      <div>
+        <InfoBar />
+        <Messages />
+        <SendMessage />
+      </div>
+    )
+  else
+    return (<ChatNotSelected />)
 }
 
-export default MessagesSection
+export default MessagesSection;
+
+const ChatNotSelected = () => {
+  return (
+    <div className='flex items-center justify-center w-full h-full'>
+			<div className='px-4 text-center sm:text-2xl md:text-3xl text-gray-700 font-semibold flex flex-col items-center gap-2'>
+				<p>Welcome 👋 TopG</p>
+				<p>Please select a chat to message</p>
+			</div>
+		</div>
+  )
+}

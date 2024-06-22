29a01a3fa10d5326ccfedb98bce01e91da2ecd02
agaberr <ahmedgaber312001@gmail.com>
2024-06-22 20:36:45 +0300

    finish message sending

--- Frontend/src/HomePage/components/MessageSection/SendMessage.jsx
+++ Frontend/src/HomePage/components/MessageSection/SendMessage.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdSend } from "react-icons/io";
+import sendMessage from "../../../hooks/sendMessage";
 
 const SendMessage = () => {
+
+  const [message, setMessage] = useState(null);
+
+  const sendmessage = sendMessage();
+  console.log(message);
+
+  const handleSendMessage = async(e) => {
+    e.preventDefault();
+    await sendmessage(message);
+    setMessage('')
+  }
+
   return (
-    <form className='flex items-center gap-2 mb-[10px] mt-1 px-5'>
-      <input type='text' placeholder='Enter Your Message' className='input input-bordered w-full bg-white text-black max-h-10' />
+    <form 
+      onSubmit={handleSendMessage}
+      className='flex items-center gap-2 mb-[10px] mt-1 px-5'>
+      <input 
+        type='text'
+        placeholder='Enter Your Message'
+        className='input input-bordered w-full bg-white text-black max-h-10'
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        />
       <button type='submit' className='absolute insert-y-0 end-5 flex items-center pe-3'>
          <IoMdSend className='font-bold size-6 text-gray-700'/>
       </button>
--- Frontend/src/hooks/sendMessage.js
+++ Frontend/src/hooks/sendMessage.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { toast } from 'react-hot-toast'
+import { useConversation } from '../context/useConversation' 
+import { useAuthContext } from '../context/AuthContext' 
 
 const sendMessage = () => {
-  return (
-    <div>sendMessage</div>
-  )
+  
+  const { messages, setMessages, selectedConversation } = useConversation();
+  // const { authUser } = AuthContext();
+
+
+  const sendmessage = async (messageSent) => {
+
+    try {
+
+      if (!messageSent || !messageSent.trim()) {
+        toast.error("Message content is required");
+        return;
+      }
+
+      const req = await fetch(`/api/message/send/${selectedConversation._id}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ "content": messageSent }),
+        });
+    
+        if (!req.ok) {
+        toast.error("Couldn't send message successfully");
+        return;
+        }
+
+    }catch(err) {
+      ast.error("Internal Server Error");
+          console.log(err);
+    }
+  } 
+
+  return sendmessage;
+
 }
 
 export default sendMessage
